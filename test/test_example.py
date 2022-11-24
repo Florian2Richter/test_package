d8c74a6fd8d483659cd0b00cd0fce26ebc363d8e
Florian Bosch Richter <florian.richter4@de.bosch.com>
2022-11-24 16:32:38 +0100

    Added Testcases

--- test/test_example.py
+++ test/test_example.py
@@ -1,1 +1,15 @@
-print('hello')
+import pytest
+from example_package_flo import example
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        (2, 3),
+        (-1, 0)
+    ]
+)
+
+
+def test_add_one(value, expected):
+    ret = example.add_one(value)
+    assert ret == expected
